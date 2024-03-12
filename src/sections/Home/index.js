761920c99df59a7c71be23e4b7df796a20479016
Valerijxxx <drozdov17@icloud.com>
2024-03-12 17:45:04 +0200

    valerius

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Button from 'Components/Button'
+import Container from 'Components/Container'
+import './styles.sass'
 
 const Home = () => {
-  return <div>Home</div>;
+  const handleClick = (hash) => {
+    location.hash =  ""
+    location.hash =  hash
+  }
+  return <div className='home'> 
+            <Container className='home__container'>
+              <h1 className='home__title'>Test assignment for front-end developer</h1>
+              <div className='home__subtitle'>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</div>
+              <Button text="Sign up" handleClick={() => handleClick("#form")} />
+            </Container>
+          </div>;
 }
 
 export default Home;
+
